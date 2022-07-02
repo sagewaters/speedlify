34b8ee555705c2e032f46ca94c1e52b115a2ade9
Sage Waters <61223596+sagewaters@users.noreply.github.com>
2022-07-02 07:06:19 +0200

    Update sagewaters.js

--- _data/sites/sagewaters.js
+++ _data/sites/sagewaters.js
@@ -1,10 +1,22 @@
 module.exports = {
 	name: "Sage's Sites", // optional, falls back to object key
-	description: "The default sites that get tested",
+	description: "Sage's Site",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
 	},
 	urls: [
-		"https://sagewaters.com/"
+		"https://sagewaters.com/",
+                "https:/sagewaters.com/works/",
+                "https:/sagewaters.com/words-pictures/",
+                "https:/sagewaters.com/shapes-figures/",
+                "https:/sagewaters.com/sound-moving/",
+                "https:/sagewaters.com/about/",
+                "https:/sagewaters.com/become-a-patron/",
+		"https:/sagewaters.com/contact/",
+    		"https:/sagewaters.com/ways-means/",
+   		"https:/sagewaters.com/fallacies/",
+  		"https:/sagewaters.com/tools/",
+		// Popular Posts
+		"https://sagewaters.com/tools/wordpress-for-artists/",
 	]
 };
