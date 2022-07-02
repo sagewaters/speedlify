95480956183ae37c9891f8b3461f11243f4a3d74
Sage Waters <61223596+sagewaters@users.noreply.github.com>
2022-07-02 06:56:22 +0200

    Update sagewaters.js

--- _data/sites/sagewaters.js
+++ _data/sites/sagewaters.js
@@ -1,5 +1,5 @@
 module.exports = {
-	name: "Sage Waters", // optional, falls back to object key
+	name: "Sage's Sites", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
 		frequency: 60 * 23, // (in minutes), 23 hours
